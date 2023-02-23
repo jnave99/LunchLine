a83b30780228698e5b37ea29acd7afa2c0edfdd2
Jacob Nave <jnave@mercury.com>
2023-02-22 16:52:44 -0800

    added budget to db

--- app/Main.hs
+++ app/Main.hs
@@ -9,13 +9,13 @@
 {-# LANGUAGE MultiParamTypeClasses #-}
 {-# LANGUAGE TypeApplications #-}
 {-# LANGUAGE RecordWildCards #-}
+{-# LANGUAGE ScopedTypeVariables #-}
 
 import Control.Monad.Logger
 import Database.Persist.Sqlite
 import Control.Monad.Reader
 import Database.Persist.TH
-import Data.List (foldl')
-import Data.Maybe (fromMaybe)
+import Data.Maybe
 import Database.Esqueleto.Experimental
 
 share [mkPersist sqlSettings, mkMigrate "migrateAll"] [persistLowerCase|
@@ -23,6 +23,8 @@ LineItem
   name String
   amount Int
   deriving Show
+Budget 
+  budget Int
 |]
 
 data Env = Env { envConn :: SqlBackend }
@@ -37,26 +39,67 @@ runDB body = do
   conn <- asks envConn
   liftIO $ runSqlConn body conn
 
-weeklyBudget :: Int
-weeklyBudget = 100
+getWeeklyBudget :: (MonadIO m) => SqlPersistT m (Maybe (Entity Budget))
+getWeeklyBudget = do 
+  budget :: Maybe (Entity Budget) <- selectFirst [] []
+  pure budget
 
-spend :: Int -> LineItem -> Int
-spend n e = n - (lineItemAmount e)
+addBudget :: (MonadIO m) => Int -> SqlPersistT m BudgetId
+addBudget x = do 
+  budgetId <- insert $ Budget x
+  pure budgetId
+
+setBudget :: (MonadIO m) => Int -> BudgetId -> SqlPersistT m BudgetId
+setBudget x budgetId = do 
+  Database.Persist.Sqlite.update budgetId [BudgetBudget Database.Persist.Sqlite.=. x]
+  pure budgetId
+
+setOrAddWeeklyBudget :: (MonadIO m) => Int -> SqlPersistT m BudgetId
+setOrAddWeeklyBudget x = do 
+  currentBudget <- getWeeklyBudget 
+  case currentBudget of 
+    Nothing -> addBudget x
+    (Just (Entity budgetId _)) -> setBudget x budgetId
+
+-- spend :: Int -> LineItem -> Int
+-- spend n e = n - (lineItemAmount e)
 -- spend n (LineItem { .. }) = n - amount
 
+getLineItemTotal :: (Num a, MonadIO m, PersistField a) => SqlPersistT m a
+getLineItemTotal = selectSum $ do
+  items <- from $ table @LineItem
+  pure $ sum_ $ items ^. LineItemAmount
+ where
+  selectSum = fmap (maybe 0 (fromMaybe 0 . unValue)) . selectOne
+
+extractBudget :: Maybe (Entity Budget) -> Int 
+extractBudget Nothing = 0
+extractBudget (Just (Entity _ (Budget amount))) = amount
 
 appMain :: AppM ()
 appMain = do
-  lineItems <- runDB $ do
+  total <- runDB $ do
     insert_ $ LineItem "Pizza" 11
     insert_ $ LineItem "Burger" 12
-    selectList [] []
-  let remainingBudget = foldl' spend weeklyBudget (fmap entityVal lineItems)
+
+    budget <- liftIO $ getLine 
+    liftIO $ putStrLn $ "Weekly budget set to: " ++ budget
+
+    _ <- setOrAddWeeklyBudget (read budget :: Int)
+
+    getLineItemTotal
+
+  budget <- runDB $ getWeeklyBudget
+
+  let remainingBudget = (extractBudget budget) - total
   liftIO . putStrLn $ "Remaining Budget: " <> show remainingBudget
-  
+
 main :: IO ()
 main = do 
   runStderrLoggingT $
     withSqliteConn ":memory:" $ \conn -> do 
-      runAppM (Env conn) (runDB $ runMigration migrateAll)
-      runAppM (Env conn) appMain 
+      runAppM (Env conn) $ do 
+        runDB $ runMigration migrateAll
+        appMain
+
+        
